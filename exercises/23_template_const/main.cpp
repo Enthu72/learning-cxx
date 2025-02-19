9d962c5211ab213b6ca269dc66bdeebc829eea8c
Enthu72 <1742526106@qq.com>
2025-02-20 02:22:09 +0800

    0219_0222

--- exercises/23_template_const/main.cpp
+++ exercises/23_template_const/main.cpp
@@ -10,7 +10,10 @@ struct Tensor {
 
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
-        // TODO: 填入正确的 shape 并计算 size
+        for (unsigned int i = 0; i < N; i++) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -29,12 +32,13 @@ struct Tensor {
         return data[data_index(indices)];
     }
 
-private:
+    private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
-        for (unsigned int i = 0; i < N; ++i) {
-            ASSERT(indices[i] < shape[i], "Invalid index");
-            // TODO: 计算 index
+        unsigned int stride = 1;
+        for (int i = N - 1; i >= 0; --i) {// 将 i 改为 int 类型
+            index += indices[i] * stride;
+            stride *= shape[i];
         }
         return index;
     }
